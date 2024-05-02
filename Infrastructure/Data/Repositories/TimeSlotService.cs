c81ea26a62077f039315b1b4f2449cbbbe6e608c
Senadin Puce <senadin.puce@gmail.com>
2024-05-02 14:07:17 +0200

    Time slots service modified

--- Infrastructure/Data/Repositories/TimeSlotService.cs
+++ Infrastructure/Data/Repositories/TimeSlotService.cs
@@ -20,25 +20,31 @@ namespace Infrastructure.Data.Repositories
         public async Task<List<TimeSlot>> GetAvailableTimeSlotsAsync(TimeSlotSearchObject search)
         {
             List<Appointment> bookedAppointments = await _context.Appointments
-            .Where(x => x.StartTime.Date.CompareTo(search.Date.Date) == 0 
-                && x.BarberId == search.BarberId 
-                && x.Status == AppointmentStatus.Reserved)
-            .Include(x => x.AppointmentServices).ThenInclude(x => x.Service)
-            .ToListAsync();
+                .Where(x => x.StartTime.Date.CompareTo(search.Date.Date) == 0
+                            && x.BarberId == search.BarberId
+                            && x.Status == AppointmentStatus.Reserved)
+                .Include(x => x.AppointmentServices).ThenInclude(x => x.Service)
+                .ToListAsync();
 
-            DateTime startTime = search.Date.Date.AddHours(9);
+            DateTime now = DateTime.Now;
+            DateTime startTime = search.Date.Date > now.Date ? search.Date.Date.AddHours(9) : now;
             DateTime endTime = search.Date.Date.AddHours(18);
 
             List<TimeSlot> availableTimeSlots = new List<TimeSlot>();
 
+            int intervalMinutes = 5;
+            int minutesToAdd = intervalMinutes - (startTime.Minute % intervalMinutes);
+            startTime = startTime.AddMinutes(minutesToAdd);
+
             while (startTime < endTime)
             {
-                if (!IsTimeSlotBooked(bookedAppointments, startTime))
+                if (startTime > now && !IsTimeSlotBooked(bookedAppointments, startTime))
                 {
                     availableTimeSlots.Add(new TimeSlot { DateTime = startTime });
                 }
-                startTime = startTime.AddMinutes(5);
+                startTime = startTime.AddMinutes(intervalMinutes);
             }
+
             return availableTimeSlots;
         }
 
@@ -47,9 +53,7 @@ namespace Infrastructure.Data.Repositories
             foreach (var appointment in appointments)
             {
                 DateTime appointmentStartTime = appointment.StartTime;
-
                 DateTime appointmentEndTime = appointmentStartTime;
-
 
                 foreach (var appointmentService in appointment.AppointmentServices)
                 {
@@ -64,6 +68,7 @@ namespace Infrastructure.Data.Repositories
 
             return false;
         }
+
 
     }
 }
