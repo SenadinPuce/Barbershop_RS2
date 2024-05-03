79a841ab6eb62dd65bf80f7345e82c1e0c2a61cc
Senadin Puce <senadin.puce@gmail.com>
2024-05-03 16:46:02 +0200

    Mail service converted to web api

--- EmailNotificationService/Program.cs
+++ EmailNotificationService/Program.cs
@@ -3,99 +3,20 @@ using System.Net.Mail;
 using EasyNetQ;
 using Core;
 
-class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IBus>(serviceProvider =>
 {
-    private static CancellationTokenSource _cancellationTokenSource;
-    private static IBus _bus;
+    var hostName = Environment.GetEnvironmentVariable("RabbitMQ_HostName") ?? "localhost";
+    var userName = Environment.GetEnvironmentVariable("RabbitMQ_UserName") ?? "guest";
+    var password = Environment.GetEnvironmentVariable("RabbitMQ_Password") ?? "guest";
 
-    static async Task Main(string[] args)
-    {
-        _cancellationTokenSource = new CancellationTokenSource();
+    var connectionString = $"host={hostName};username={userName};password={password}";
+    return RabbitHutch.CreateBus(connectionString);
+});
 
-        var hostName = Environment.GetEnvironmentVariable("RabbitMQ_HostName") ?? "localhost";
-        var userName = Environment.GetEnvironmentVariable("RabbitMQ_UserName") ?? "guest";
-        var password = Environment.GetEnvironmentVariable("RabbitMQ_Password") ?? "guest";
+builder.Services.AddHostedService<RabbitMQSubscriberService>();
 
-        var connectionString = $"host={hostName};username={userName};password={password}";
+var app = builder.Build();
 
-        int currentRetry = 0;
-         _bus = RabbitHutch.CreateBus(connectionString);
-         
-        while (true)
-        {
-            try
-            {
-
-                _bus.PubSub.Subscribe<AppointmentMessage>("mail_service", SendEmailNotificationAsync);
-                Console.WriteLine("Listening for messages. Press CTRL+C to exit.");
-                break;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error connecting to RabbitMQ (Attempt {currentRetry + 1}): {ex.Message}");
-                currentRetry++;
-                await Task.Delay(TimeSpan.FromSeconds(Math.Pow(2, currentRetry)));
-            }
-        }
-
-        Console.CancelKeyPress += (sender, e) =>
-               {
-                   e.Cancel = true;
-                   _cancellationTokenSource.Cancel();
-               };
-
-        await WaitForCancellationAsync();
-    }
-    static async Task WaitForCancellationAsync()
-    {
-        try
-        {
-            await Task.Delay(Timeout.Infinite, _cancellationTokenSource.Token);
-        }
-        catch (OperationCanceledException)
-        {
-            Console.WriteLine("Application is shutting down...");
-        }
-    }
-
-    static async Task SendEmailNotificationAsync(AppointmentMessage appointment)
-    {
-        Console.WriteLine("Message received");
-
-        try
-        {
-            string senderEmail = Environment.GetEnvironmentVariable("OUTLOOK_MAIL") ?? "barbershop_rs2@outlook.com";
-            string senderPassword = Environment.GetEnvironmentVariable("OUTLOOK_PASS") ?? "Barbershop!";
-
-            using var client = new SmtpClient("smtp.office365.com", 587);
-            client.EnableSsl = true;
-            client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential(senderEmail, senderPassword);
-
-            var message = new MailMessage(
-                from: senderEmail,
-                to: appointment.ClientEmail,
-                subject: "Appointment Notification",
-                body: $"We are happy to inform you that your appointment is confirmed! \n\n" +
-                      $"Appointment Details:\n" +
-                      $"Service: {appointment.Service}\n" +
-                      $"Barber: {appointment.BarberFullName}\n" +
-                      $"Date and Time: {appointment.DateTime}");
-
-            await client.SendMailAsync(message);
-            Console.WriteLine("Email notification sent successfully.");
-        }
-        catch (SmtpException ex)
-        {
-            Console.WriteLine($"SMTP error sending email: {ex.Message}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error sending email notification: {ex.Message}");
-        }
-        finally
-        {
-            Console.WriteLine("Listening for messages. Press CTRL+C to exit.");
-        }
-    }
-}
+app.Run();
---
+++ EmailNotificationService/RabbitMQSubscriberService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using EasyNetQ;
+using Core;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class RabbitMQSubscriberService : BackgroundService
+{
+	private readonly  IBus _bus;
+
+	public RabbitMQSubscriberService(IBus bus)
+	{
+		_bus = bus;
+	}
+
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		while (true)
+		{
+			try
+			{
+				_bus.PubSub.Subscribe<AppointmentMessage>("mail_service", async appointment => await HandleMessageAsync(appointment));
+				Console.WriteLine("Listening for messages.");
+				break;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"An error occurred while subscribing to RabbitMQ: {ex.Message}");
+
+			}
+		}
+		await Task.CompletedTask;
+	}
+
+	private async Task HandleMessageAsync(AppointmentMessage appointment)
+	{
+		Console.WriteLine($"Received appointment message for client: {appointment.ClientEmail}");
+
+		try
+		{
+			string senderEmail = Environment.GetEnvironmentVariable("Outlook_Mail") ?? "barbershop_rs2@outlook.com";
+			string senderPassword = Environment.GetEnvironmentVariable("Outlook_Password") ?? "Barbershop!";
+
+			using var client = new SmtpClient("smtp.office365.com", 587)
+			{
+				EnableSsl = true,
+				UseDefaultCredentials = false,
+				Credentials = new NetworkCredential(senderEmail, senderPassword)
+			};
+
+			var message = new MailMessage(
+				from: senderEmail,
+				to: appointment.ClientEmail,
+				subject: "Appointment Notification",
+				body: $"We are happy to inform you that your appointment is confirmed! \n\n" +
+					  $"Appointment Details:\n" +
+					  $"Service: {appointment.Service}\n" +
+					  $"Barber: {appointment.BarberFullName}\n" +
+					  $"Date and Time: {appointment.DateTime}");
+
+			await client.SendMailAsync(message);
+			Console.WriteLine("Email notification sent successfully.");
+		}
+		catch (SmtpException ex)
+		{
+			Console.WriteLine($"SMTP error sending email: {ex.Message}");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error sending email notification: {ex.Message}");
+		}
+	}
+}
